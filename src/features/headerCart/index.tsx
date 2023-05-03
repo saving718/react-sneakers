9efee571b80fb7906811818119075b3fb66349cb
ivanov <ivanoffffff01@gmail.com>
2023-05-03 13:39:52 +0300

    animation

--- src/features/headerCart/index.tsx
+++ src/features/headerCart/index.tsx
@@ -3,11 +3,37 @@ import { useSelector } from "react-redux";
 import styles from "./headerCart.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import anime from "animejs";
 import cart from "app/assets/image/cart.png";
 
 const HeaderCart = () => {
 	const selectPrice: string = useSelector((state: any) => state.cart.fullPrice);
+	const animation = React.useRef(null);
+	const selectPriceRef = React.useRef(null);
 	
+	const [price1, setPrice1] = React.useState(1);
+	React.useEffect(() => {
+		animation.current = anime.timeline({
+			direction: "alternate",
+			loop: false,
+			autoplay: true,
+			easing: "easeInOutSine"
+		  });
+		  
+		animation.current.add({
+			targets: selectPrice,
+			price: selectPrice,
+			round: 1,
+			easing: "linear",
+			update: function() {
+				setPrice1(selectPrice);
+			}
+		});
+		
+		console.log(animation);
+		
+	}, [selectPrice]);
+
 	return (
 		<div className={styles["cart"]}>
 			<div>
@@ -22,7 +48,9 @@ const HeaderCart = () => {
 								height="28px"
 							/>
 						</span>
-						<span className={styles["cart__price"]}>{selectPrice}</span>
+						<span ref={selectPriceRef} className={styles["cart__price"]}>
+							{JSON.stringify(price1)}
+						</span>
 					</a>
 				</Link>
 			</div>
